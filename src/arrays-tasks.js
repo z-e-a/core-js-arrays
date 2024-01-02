cd380284fabab15d8f6af343f98e7203530b26fa
z-e-a <e.a.zubkov@yandex.ru>
2024-01-02 21:31:30 +0300

    feat: solve task 'findAllOccurrences'

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -100,8 +100,8 @@ function findElement(arr, value) {
  *    findAllOccurrences([ null, undefined, null ], null) => 2
  *    findAllOccurrences([ true, 0, 1, 'true' ], true) => 1
  */
-function findAllOccurrences(/* arr, item */) {
-  throw new Error('Not implemented');
+function findAllOccurrences(arr, item) {
+  return arr.reduce((prev, curr) => prev + (curr === item ? 1 : 0), 0);
 }
 
 /**
