88226bba25e5dacd229b373227189ab095827967
z-e-a <e.a.zubkov@yandex.ru>
2024-01-16 17:09:22 +0300

    feat: solve task 'createNDimensionalArray'

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -289,10 +289,14 @@ function distinct(arr) {
  *    createNDimensionalArray(4, 2) => [[[[0, 0], [0, 0]], [[0, 0], [0, 0]]], [[[0, 0], [0, 0]], [[0, 0], [0, 0]]]]
  *    createNDimensionalArray(1, 1) => [0]
  */
-function createNDimensionalArray(/* n, size */) {
-  throw new Error('Not implemented');
+function createNDimensionalArray(n, size) {
+  if (n <= 1) {
+    return Array(size).fill(0);
+  }
+  return Array(size)
+    .fill(0)
+    .map(() => createNDimensionalArray(n - 1, size));
 }
-
 /**
  * Flattens a nested array into a single-level array.
  *
