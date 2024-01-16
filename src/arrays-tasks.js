cb3464bdb9226ab943316a7d9fc2e6866f641c43
z-e-a <e.a.zubkov@yandex.ru>
2024-01-16 18:51:39 +0300

    feat: solve task 'calculateBalance'

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -344,8 +344,8 @@ function selectMany(arr, childrenSelector) {
  *   calculateBalance([ [ 10, 8 ], [ 1, 5 ] ])  => (10 - 8) + (1 - 5) = 2 + -4 = -2
  *   calculateBalance([]) => 0
  */
-function calculateBalance(/* arr */) {
-  throw new Error('Not implemented');
+function calculateBalance(arr) {
+  return arr.reduce((bal, el) => bal + el[0] - el[1], 0);
 }
 
 /**
