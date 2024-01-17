edcc71a2eccb4c6b33ec8d77f68c7ab8bb7293b8
z-e-a <e.a.zubkov@yandex.ru>
2024-01-17 17:38:30 +0300

    feat: solve task 'getIndicesOfOddNumbers'

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -455,8 +455,11 @@ function getIdentityMatrix(n) {
  *    getIndicesOfOddNumbers([2, 4, 6, 8, 10]) => []
  *    getIndicesOfOddNumbers([11, 22, 33, 44, 55]) => [0, 2, 4]
  */
-function getIndicesOfOddNumbers(/* numbers */) {
-  throw new Error('Not implemented');
+function getIndicesOfOddNumbers(numbers) {
+  return numbers.reduce(
+    (result, num, index) => (num % 2 === 0 ? result : [...result, index]),
+    []
+  );
 }
 
 /**
