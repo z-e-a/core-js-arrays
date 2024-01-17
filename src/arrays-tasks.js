9005c5769893cf62af0ed62715f82317301bc975
z-e-a <e.a.zubkov@yandex.ru>
2024-01-17 18:13:47 +0300

    feat: solve task 'findCommonElements'

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -506,8 +506,8 @@ function getMaxItems(arr, n) {
  *    findCommonElements(['a', 'b', 'c'], ['b', 'c', 'd']) => [ 'b', 'c' ]
  *    findCommonElements([1, 2, 3], ['a', 'b', 'c']) => []
  */
-function findCommonElements(/* arr1, arr2 */) {
-  throw new Error('Not implemented');
+function findCommonElements(arr1, arr2) {
+  return arr1.filter((el) => arr2.includes(el));
 }
 
 /**
