e33b84c42d9f4bcb528a4ed6ea3aae1ef0ca7e7e
z-e-a <e.a.zubkov@yandex.ru>
2024-01-17 17:21:38 +0300

    feat: solve task 'generateOdds'

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -383,8 +383,8 @@ function createChunks(arr, chunkSize) {
  *    generateOdds(2) => [ 1, 3 ]
  *    generateOdds(5) => [ 1, 3, 5, 7, 9 ]
  */
-function generateOdds(/* len */) {
-  throw new Error('Not implemented');
+function generateOdds(len) {
+  return Array.from({ length: len }, (_, ind) => ind * 2 + 1);
 }
 
 /**
