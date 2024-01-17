7219860688e08464ef72d536f70bac88ea6f7e13
z-e-a <e.a.zubkov@yandex.ru>
2024-01-17 18:11:18 +0300

    feat: solve task 'getMaxItems'

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -490,8 +490,8 @@ function getHexRGBValues(arr) {
  *   getMaxItems([ 10, 2, 7, 5, 3, -5 ], 3) => [ 10, 7, 5 ]
  *   getMaxItems([ 10, 10, 10, 10 ], 3) => [ 10, 10, 10 ]
  */
-function getMaxItems(/* arr, n */) {
-  throw new Error('Not implemented');
+function getMaxItems(arr, n) {
+  return arr.sort((a, b) => b - a).filter((el, ind) => ind < n);
 }
 
 /**
