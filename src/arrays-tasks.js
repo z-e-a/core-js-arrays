af682128ccc91a1991c0be72086d5cdaa6d770f2
z-e-a <e.a.zubkov@yandex.ru>
2024-01-17 17:52:39 +0300

    feat: solve task 'getHexRGBValues'

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -472,8 +472,8 @@ function getIndicesOfOddNumbers(numbers) {
  *    getHexRGBValues([ 0, 255, 16777215]) => [ '#000000', '#0000FF', '#FFFFFF' ]
  *    getHexRGBValues([]) => []
  */
-function getHexRGBValues(/* arr */) {
-  throw new Error('Not implemented');
+function getHexRGBValues(arr) {
+  return arr.map((el) => `#${el.toString(16).toUpperCase().padStart(6, '0')}`);
 }
 
 /**
