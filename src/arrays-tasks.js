f3746ffa16ff2b9abf5d122605ae4c326ceded63
z-e-a <e.a.zubkov@yandex.ru>
2024-01-18 11:00:38 +0300

    feat: solve task 'shiftArray'

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -564,10 +564,9 @@ function propagateItemsByPositionIndex(arr) {
  *    shiftArray(['a', 'b', 'c', 'd'], -1) => ['b', 'c', 'd', 'a']
  *    shiftArray([10, 20, 30, 40, 50], -3) => [40, 50, 10, 20, 30]
  */
-function shiftArray(/* arr, n */) {
-  throw new Error('Not implemented');
+function shiftArray(arr, n) {
+  return n > 0 ? arr.splice(-n, n).concat(arr) : arr.concat(arr.splice(0, -n));
 }
-
 /**
  * Sorts digit names.
  *
